e6cceada60a2e56d7b1ae2b6f9c8ba4059d6a4a2
EnriqueLabradaRivero <136727587+EnriqueLabradaRivero@users.noreply.github.com>
2024-05-14 14:33:55 -0400

    add personal links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.js'
+import'../node_modules/bootstrap-icons/font/bootstrap-icons.css'
 import TextArea from './components/text-area.jsx'
 import VistaPrevia from './components/vistaPrevia.jsx';
 
@@ -9,7 +10,14 @@ function App() {
     <div className='App container d-flex'>
       <TextArea></TextArea>
       <VistaPrevia></VistaPrevia>
-      <footer>Created by Enrique Labrada</footer>
+      <footer>
+      <p> Created by EnriqueLabrada </p>
+        <div className='enlaces d-flex'>
+          <a href='https://github.com/EnriqueLabrada' rel='noreferrer' target='_blank'><i className='bi bi-github'></i></a>
+          <a href='https://www.linkedin.com/in/enrique-labrada-rivero-814b4927a/' rel='noreferrer' target='_blank'><i className='bi bi-linkedin'></i></a>
+          <a href='https://www.instagram.com/enri_labrada?igsh=MWdnMHZobXNmZGVjbQ==' rel='noreferrer' target='_blank'><i class="bi bi-instagram"></i></a>
+        </div>
+      </footer>
     </div>
   );
 }
